498795d859dd74b340149789b8103ddec76421db
siliang0426 <SIL71@pitt.edu>
2023-09-16 14:46:17 -0400

    API update

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 from pymongo import MongoClient
 from flask_session import Session
+import re
 
 app = Flask(__name__)
 app.secret_key="12345"
@@ -10,6 +11,38 @@ CORS(app)
 client = MongoClient("mongodb+srv://siliang:AA6fDGeHzpg32wMm@communication.qdhodth.mongodb.net/?retryWrites=true&w=majority")
 db = client["Communication"]
 user_collection = db["User"]
+
+#writing stopword
+stopword = []
+stopwords_file_path="reasources/stopwords.txt"
+with open(stopwords_file_path, 'r') as f:
+    for line in f:
+    # Tokenize the line into words.
+        words = line.strip().split()
+    # Update word frequencies.
+        for word in words:
+            stopword.append(word.lower())
+    
+
+def get_keywords_from_file(filename):
+    HiFreqWord = {}
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+        words = re.split(r'[ .,!;\n\t]+', text)
+        words = [word for word in words if word]
+        for word in words:
+            if word.lower() in HiFreqWord:
+                HiFreqWord[word.lower()]+=1
+            else:
+                HiFreqWord[word.lower()]=1
+                
+    filtered_HiFreqWord = {k: v for k, v in HiFreqWord.items() if k not in stopword}
+    sorted_HiFreqWord = sorted(filtered_HiFreqWord.items(), key=lambda x: x[1], reverse=True)
+    top_10_HiFreqWord = sorted_HiFreqWord[:10]
+   
+    return top_10_HiFreqWord[:10]  # Take first 10 keywords
+
 
 @app.route('/signup',methods=['POST'])
 def signUp():
@@ -23,7 +56,12 @@ def signUp():
                 return jsonify({'status': "status","message":'Missing data'}), 400
             
             if username and password:
-                user_collection.insert_one({"Username": username, "PassWord": password})
+                user_collection.insert_one({
+                    "Username": username,
+                    "PassWord": password
+                    })
+                
+                
                 return jsonify({"status": "success","message":"SignUp Success"}), 200
             else:
                 return jsonify({"status": "failure", "error": "Missing fields"}), 400
@@ -48,6 +86,11 @@ def login():
                 return jsonify({"status": "failure", "error": "No User"}), 400
     except Exception as e:
         return jsonify({"status": "failure", "error": str(e)}), 500
+
+@app.route('/patient_history',methods=['POST'])
+def patient_history():
+    
+    return jsonify({"status": "failure", "error": "not implemented yet"}), 500
 
 
 if __name__ == '__main__':
